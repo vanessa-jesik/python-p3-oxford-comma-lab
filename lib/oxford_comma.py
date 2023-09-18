3a3bc8b7555182bc21e3f34269c12301c40dae87
Vanessa Jesik <vajesik@gmail.com>
2023-09-18 16:30:01 -0600

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
